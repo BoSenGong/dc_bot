1349fa0cda76a966398ac7b244f07ceeb6199939
BoSenGong <bosengong@gmail.com>
2022-01-19 00:39:17 +0800

    Add files via upload

--- bot.py
+++ bot.py
@@ -2,27 +2,29 @@ import os
 import json
 import discord
 from discord.ext import commands
-
+os.chdir(r'C:\discord_bot\whitelist')
+for filename in os.listdir('./cmds'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cmds.{filename[:-3]}')
 with open('setting.json','r',encoding='utf8') as jfile:
-    jdata=json.load(jfile)
+    setting=json.load(jfile)
 
 intents = discord.Intents.default()
 intents.members = True
 bot = commands.Bot(command_prefix='!', intents=intents)
-os.chdir(r'C:\discord_bot\whitelist')
 
-#bot ready event
+### bot ready event ###
 @bot.event
 async def on_ready():
     print(">>> AcaPunks Bot is online. <<<")
 
-#member join event
+### member join event ###
 @bot.event
 async def on_member_join(member):
-    channel = bot.get_channel(jdata['welcome_channel'])
+    channel = bot.get_channel(setting['welcome_channel'])
     await channel.send(f"{member} join!")
 
-#message event###
+### message event ###
 @bot.event
 async def on_message(message):
     #check message.author != bot
@@ -30,20 +32,20 @@ async def on_message(message):
         return
 
     #Listen to whitelist channel then copy address
-    if (message.channel.id == jdata['whitelist_channel']):        
+    if (message.channel.id == setting['whitelist_channel']):        
         #Check address
         if message.content.startswith('0x'):  
             msg = message.content
             await message.delete()
             await message.channel.send(f"{message.author.mention} address received.")
             #Go to private address channel
-            channel = bot.get_channel(jdata['whitelist_private_channel']) 
+            channel = bot.get_channel(setting['whitelist_private_channel']) 
             await channel.send(f"{message.author.mention}  "+msg)
         else:
             await message.delete()
             await message.channel.send(f"{message.author.mention} Please leave your address only.")   
 
-###Count level and experience###
+    #Count level and experience
     with open('users.json','r') as f :
         users = json.load(f)
         await update_data(users, message.author)
@@ -67,14 +69,10 @@ async def level_up(users,user,channel):
     lvl_start = users[str(user.id)]['level']
     lvl_end = int(experience**(1/4))
     if lvl_start < lvl_end:
-        channel = bot.get_channel(jdata['level_channel']) 
+        channel = bot.get_channel(setting['level_channel']) 
         await channel.send('{} has leveled up to level {}'.format(user.mention,lvl_end))
         users[str(user.id)]['level'] = lvl_end
-###Count level and experience end###   
-
-for filename in os.listdir('./cmds'):
-    if filename.endswith('.py'):
-        bot.load_extension(f'cmds.{filename[:-3]}')
+#Count level and experience end   
 
 if __name__ == "__main__":
-    bot.run(jdata['token'])
+    bot.run(setting['token'])
